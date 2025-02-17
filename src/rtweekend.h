e8c688e9961be11ad0cbd8b6e2ab1090f3334471
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-02-17 23:09:51 +0100

    Create the common header

---
+++ src/rtweekend.h
@@ -0,0 +1,32 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+
+// C++ Std Usings
+
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = M_PI;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common Headers
+
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+#endif // !RTWEEKEND_H
