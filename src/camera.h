3fe8e6911c98377cfb75d32d138da42a297ca595
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-05-05 20:39:10 +0200

    Create the material class and add Lambertian and Metal

--- src/camera.h
+++ src/camera.h
@@ -5,6 +5,7 @@
 
 #include "rtweekend.h"
 #include "hittable.h"
+#include "material.h"
 #include "color.h"
 #include "ray.h"
 #include "interval.h"
@@ -100,8 +101,11 @@ private:
         hit_record rec;
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth-1, world);
+            return color(0,0,0);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,7 @@
 #include "color.h"
 #include "hittable.h"
 #include "hittable_list.h"
+#include "material.h"
 #include "ray.h"
 #include "sphere.h"
 #include "vec3.h"
@@ -12,8 +13,15 @@
 int main() {
     hittable_list world;
 
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     camera cam;
 
