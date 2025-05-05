080602a64114cae65dd6b418e75c6f958737f255
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-05-06 00:18:38 +0200

    Ajust camera field-of-view

--- src/camera.h
+++ src/camera.h
@@ -18,6 +18,8 @@ public:
     int    samples_per_pixel = 10;   // Count of random samples for each pixel
     int    max_depth         = 10;   // Maximum number of ray bounces into scene
 
+    double vfov = 90;  // Vertical view angle (field of view)
+
     void render(const hittable& world) {
         initialize();
 
@@ -56,7 +58,9 @@ private:
 
         // Determine viewport dimensions.
         auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+        auto theta = degrees_to_radians(vfov);
+        auto h = std::tan(theta/2);
+        auto viewport_height = 2 * h * focal_length;
         auto viewport_width = viewport_height * (double(image_width)/image_height);
 
         // Calculate the vectors across the horizontal and down the vertical viewport edges.
--- src/main.cc
+++ src/main.cc
@@ -25,12 +25,22 @@ int main() {
     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
+    // auto R = std::cos(pi/4);
+
+    // auto material_left  = make_shared<lambertian>(color(0,0,1));
+    // auto material_right = make_shared<lambertian>(color(1,0,0));
+
+    // world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
+    // world.add(make_shared<sphere>(point3( R, 0, -1), R, material_right));
+
     camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
+
+    cam.vfov = 90;
 
     cam.render(world);
 }
