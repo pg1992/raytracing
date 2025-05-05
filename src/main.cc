3e9d404bd7b2971a87883bd4646c6642de81efa1
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-05-05 18:13:19 +0200

    Render diffuse materials

--- src/main.cc
+++ src/main.cc
@@ -17,9 +17,10 @@ int main() {
 
     camera cam;
 
-    cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 400;
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
     cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
 
     cam.render(world);
 }
